eae93301b3da60c7664b196dd41ad137f9efc743
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-05-08 17:19:49 +0000

    class of journal log started

---
+++ log.rb
@@ -0,0 +1,14 @@
+class Log
+  attr_accessor: :species, :date, :location
+
+  def initialize(species, date, location)
+    @species = species
+    @date = date
+    @location = location
+  end
+
+  def to_s
+    return "#{@species} | #{@date} | @{location} "
+  end
+
+end

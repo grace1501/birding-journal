008046d3faafbaf3243b728d581060c97f6c47e0
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-05-13 03:53:48 +0000

    test Log class

--- tests/test_log.rb
+++ tests/test_log.rb
@@ -4,6 +4,6 @@ require './log'
 class TestLog < Minitest::Test
   def test_log
     hawk_log = Log.new("hawk", "2024/05/08", "city park")
-    assert_equal ''
+    assert_equal 'hawk | 2024/05/08 | city park', hawk_log.to_s, "Log class to_s method fail"
   end
 end
